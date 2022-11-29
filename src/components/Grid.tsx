e948a3480c2803d928a451ca5fcf899ba67e6963
Daniele Megna <daniele.megna@casavo.com>
2022-11-29 12:24:01 +0100

    Extracted gridSizeRange function from Grid component

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,28 +1,29 @@
+import * as _ from "lodash"
 import { Component } from "solid-js"
 import { AliveCells, includes } from "../core/AliveCells"
 import { Coordinate } from "../core/Coordinate"
 import { Boundaries } from "../core/Game"
 import styles from "./style.module.css"
-import * as _ from "lodash"
 
 interface Props {
   aliveCells: AliveCells
   boundaries: Boundaries
 }
 
+const gridSizeRange = (boundaries: Boundaries) => {
+  return _.range(
+    Math.min(boundaries?.left ?? 0, boundaries?.bottom ?? 0),
+    Math.max(boundaries?.right ?? 0, boundaries?.top ?? 0) + 1
+  )
+}
+
 export const Grid: Component<Props> = (props) => {
-  const gridSize = () => {
-    return _.range(
-      Math.min(props.boundaries?.left ?? 0, props.boundaries?.bottom ?? 0),
-      Math.max(props.boundaries?.right ?? 0, props.boundaries?.top ?? 0)
-    )
-  }
   return (
     <table class={styles.gridTable}>
       <tbody>
-        {gridSize().map((y: number) =>
+        {gridSizeRange(props.boundaries).map((y: number) =>
           <tr>
-            {gridSize().map((x: number) => {
+            {gridSizeRange(props.boundaries).map((x: number) => {
               const current: Coordinate = [x, y]
               const alive = includes(props.aliveCells, current)
               return <td classList={{ [styles.aliveCell]: alive, [styles.deadCell]: !alive }}></td>
